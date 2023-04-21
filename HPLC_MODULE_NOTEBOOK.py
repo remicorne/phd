a49bd9807b04469fd9dfae56aad00afca516c20c
jjb-hub <120100465+jjb-hub@users.noreply.github.com>
2023-04-21 18:03:13 +0200

    finalized generic apply to column util

--- HPLC_MODULE_NOTEBOOK.py
+++ HPLC_MODULE_NOTEBOOK.py
@@ -3,6 +3,6 @@
 
 ######## GENERIC UTILITARIES ############
 def applyMappingToColumn(df, column, mapping):
-    df[column] = int(df[column])
+    df[column] = df[column].astype(int)
     df[column] = df.apply( lambda x: mapping[x[column]], axis = 1)
     return df
