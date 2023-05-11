0b771eb332679fb5705a044db31cdf27766eff1c
remicorne <remi.corne@gmail.com>
2023-05-11 09:32:45 +0200

    Change mous_no, group_no to id, put all treatments of 1 experiment on same correlogram,

--- HPLC_MODULE_NOTEBOOK.py
+++ HPLC_MODULE_NOTEBOOK.py
@@ -46,7 +46,7 @@ INPUT_DIR = f'{ROOT}/input'
 OUTPUT_DIR = f'{ROOT}/output'
 CACHE_DIR = f'{INPUT_DIR}/cache'
 
-COLUMN_ORDER = {'BR':['OF', 'PL', 'CC', 'IC', 'M', 'SJ', 'SL1', 'SR1', 'SL6', 'SR6', 'AC', 'V' , 
+COLUMN_ORDER = {'region':['OF', 'PL', 'CC', 'IC', 'M', 'SJ', 'SL1', 'SR1', 'SL6', 'SR6', 'AC', 'V' , 
             'Am', 'dH', 'vH', 'NAc', 'VM', 'DM', 'VL', 'DL', 'MD', 'VPL', 'VPR', 'DG', 
             'Y', 'SC', 'SN', 'VTA', 'DR', 'MR', 'CE' ],
                    'compound': ['DA', 'DOPAC', 'HVA', '3MT', '5HT', '5HIAA', 'GLU', 'GLN']}
@@ -106,9 +106,14 @@ def getCache(filename, df_identifier):
 def isCached(filename, df_identifier):
     return os.path.isfile(f'{CACHE_DIR}/{filename}/{df_identifier}.pkl')
 
-def applyTreatmentMapping(df, mapping):
-    df['treatment'] = df.apply(lambda x: mapping[str(int(x['group_no']))], axis=1)
-    return df
+def applyTreatmentMapping(df, filename):
+    treatment_mapping_path = f"{CACHE_DIR}/{filename.split('.')[0]}/treatment_mapping.json"
+    if not os.path.isfile(treatment_mapping_path): #Check treatment mapping is present
+        raise Exception("TREATMENT INFORMATION ABSENT, TO SAVE TREATMENT MAPPING RUN 'setTreatment(filename, treatment_mapping)'")
+    treatment_mapping = getJSON((treatment_mapping_path))
+    new_columns = list(list(treatment_mapping.values())[0].keys()) # Get the future column names from one of the treatments
+    df[new_columns] = df.apply(lambda x: treatment_mapping[str(int(x['group_id']))].values(), axis=1, result_type='expand') # Get alll the values and assign to corresponding columns
+    return df.explode('experiments').rename(columns={'experiments':'experiment'}) # Duplicate rows belonging to multiple experiment so that groupby can be done later
     
 def dictToFilename(dict_to_stringify):
     result = str(dict_to_stringify)
@@ -155,11 +160,6 @@ def getAggregateStatsDf(filename, df_type):
     if df_type not in ['ratio', 'compound']: raise Exception("DF TYPE MUST BE IN ['ratio', 'compound']")
     builder_callback_with_param = functools.partial(buildAggregateStatsDf, df_type=df_type)
     return getOrBuildDf(filename, f"{df_type}_aggregate_stats", builder_callback_with_param)
-    
-#Generic function to select any ratio imaginable and return the corresponding df
-def selectRatio(region_1, region2, compound_1, compound2, ratios_df):
-    return ratios_df.loc[(ratios_df['BR_1']==region_1) & (ratios_df['compound_1']==compound_1) & (ratios_df['BR_2']==region_2) & (ratios_df['compound_2']==compound_2)]
-
 
 
 ############ BUILDERS #########
@@ -171,42 +171,24 @@ def buildRawDf(filename):
         raise Exception(f'METHOD TO DEAL WITH FILE TYPE "{file_type}" ABSENT')
     if not os.path.isfile(f'{INPUT_DIR}/{filename}'):
         raise Exception(f'FILE {filename} IS ABSENT IN "input/" DIRECTORY')
-    raw_df = pd.read_csv(f'{INPUT_DIR}/{filename}', header=0).replace(np.nan, 0) # to set all 0 to Nan
-    if not os.path.isfile(f"{CACHE_DIR}/{file_name}/treatment_mapping.json"): #Check treatment mapping is present
-        raise Exception("TREATMENT INFORMATION ABSENT, TO SAVE TREATMENT MAPPING RUN 'setTreatment(filename, treatment_mapping)'")
-    treatment_mapping = getJSON((f"{CACHE_DIR}/{file_name}/treatment_mapping.json"))
-    return applyTreatmentMapping(raw_df, treatment_mapping)
+    return pd.read_csv(f'{INPUT_DIR}/{filename}', header=0).replace(np.nan, 0).rename(columns={'mouse_no':'mouse_id', 'group_no':'group_id'}) # to set all 0 to Nan
 
 #contains the logic to build the df in the new format based on raw df
 def buildCompoundDf(filename):
     raw_df = getRawDf(filename)
-    col_names = ['mouse_id' , 'treatment' , 'BR' , 'compound' , 'ng_mg']
-    
+    new_format_df = raw_df.melt(id_vars=['mouse_id', 'group_id'], value_vars=raw_df.columns[2:])
+    new_format_df[['compound', 'region']] = new_format_df.apply(lambda x: x.variable.split('_'), axis=1, result_type='expand')
+    new_format_df = applyTreatmentMapping(new_format_df, filename)
+    return new_format_df.rename(columns={'value': 'ng_mg'}).drop(columns=['variable'])
 
-    raw_col_names = raw_df.columns.copy()
-    result_rows = []
-
-    for ind, row in raw_df.iterrows(): #loop for row get mouse and group
-        
-        mouse_id = row[0]
-        treatment = row[1]
-
-        for val, col_name in zip(row[2:], raw_col_names[2:]): #loop within row
-
-            if val > 0:
-                compound, BR = col_name.split('_')
-                result_rows.append([mouse_id, treatment, BR, compound, val])
-    
-    return pd.DataFrame(result_rows, columns=col_names)
-    
 #Contains the logic to build the ratios df based on the df with the new format
 def buildRatiosDf(filename):
     compound_df = getCompoundDf(filename) #.iloc[0:100] #To speed up testing
-    merged = pd.merge(left=compound_df, right=compound_df, on='mouse_id', suffixes=['_1', '_2']) #merge every BR/compound combo to every other for each mouse
-    merged = merged[~((merged.BR_1 == merged.BR_2) & (merged.compound_1 == merged.compound_2))] #eliminate duplicates ((BR1 == BR2 & C1 == C2)
-    # merged = merged[(merged.BR_1 < merged.BR_2) | (merged.compound_1 < merged.compound_2) #eliminate duplicates ((BR1 == BR2 & C1 == C2) and cross combinations (row1: BR1=A, C1=B, BR2=C, C2=D; row2: BR1=C, C1=D, BR2=A, C2=B))
-    #         & ~((merged.BR_1 > merged.BR_2) & (merged.compound_1 < merged.compound_2))] #Uncomment code to only save half the ration (C1/C2) to divide time it takes by 2
-    merged[['compounds', 'ratio']] = merged.apply(lambda x: [f'{x.compound_1}/{x.compound_2}', x.ng_mg_1 / x.ng_mg_2], axis=1, result_type='expand') #calculate the ratio
+    merged = pd.merge(left=compound_df, right=compound_df, on='mouse_id', suffixes=['_1', '_2']) #merge every region/compound combo to every other for each mouse
+    merged = merged[~((merged.region_1 == merged.region_2) & (merged.compound_1 == merged.compound_2))] #eliminate duplicates (region1 == region2 & C1 == C2)
+    # merged = merged[(merged.region_1 < merged.region_2) | (merged.compound_1 < merged.compound_2) #Eliminate duplicates (region1 == region2 & C1 == C2) and cross combinations (row1: region1=A, C1=B, region2=C, C2=D; row2: region1=C, C1=D, region2=A, C2=B))
+    #         & ~((merged.region_1 > merged.region_2) & (merged.compound_1 < merged.compound_2))] #Uncomment code to only save half the ration (C1/C2) to divide time it takes by 2
+    merged[['compounds', 'ratio']] = merged.apply(lambda x: [f'{x.compound_1}/{x.compound_2}', x.ng_mg_1 / x.ng_mg_2 if x.ng_mg_2 else np.NaN], axis=1, result_type='expand') #calculate the ratio
     return merged.rename(columns={'treatment_1': 'treatment'}).drop(columns=['treatment_2', 'ng_mg_1', 'ng_mg_2']) #Drop duplicate columns
 
 #Function to get the specific rations (intra region) that you use based on a ratios dictionnary
@@ -218,39 +200,46 @@ def buildRatiosPerRegionDf(filename, ratios_mapping):
             compound_ratios.append(ratios_df[(ratios_df['compound_1']==compound_1) & (ratios_df['compound_2']==compound_2)])
     return pd.concat(compound_ratios)
     
-#returns df columns = ['treatment', 'BR', 'compound', 'F_value', 'p_value']
+#returns df columns = ['treatment', 'region', 'compound', 'F_value', 'p_value']
 def buildAggregateStatsDf(filename, df_type):
     working_df = getCompoundDf(filename) if df_type == 'compound' else getRatiosDf(filename)
     result_ls = []
-    for treat_BR_comp, groupby_df in working_df.groupby(by =['treatment', 'BR', 'compound']):
+    for treat_region_comp, groupby_df in working_df.groupby(by =['treatment', 'region', 'compound']):
         F, p, is_valid = [*scipy.stats.shapiro(groupby_df['ng_mg']), True] if len(groupby_df) >= 3 else [np.NaN, np.NaN, False]
         mean, std, sem, values = [groupby_df.ng_mg.mean(), groupby_df.ng_mg.std(), groupby_df.ng_mg.sem(), list(groupby_df.ng_mg)]  
-        result_ls.append([*treat_BR_comp, F, p, is_valid, mean, std, sem, values]) #start unpacks the list of strings
-    return pd.DataFrame(result_ls, columns= ['treatment', 'BR', 'compound', 'shapiro_F', 'shapiro_p', 'is_valid', 'mean', 'std', 'sem', 'values'])
+        result_ls.append([*treat_region_comp, F, p, is_valid, mean, std, sem, values]) #start unpacks the list of strings
+    return pd.DataFrame(result_ls, columns= ['treatment', 'region', 'compound', 'shapiro_F', 'shapiro_p', 'is_valid', 'mean', 'std', 'sem', 'values'])
 
-#returns df columns = ['treatment', 'BR', 'compound', 'F_value', 'p_value']
+#returns df columns = ['treatment', 'region', 'compound', 'F_value', 'p_value']
 def testBuildAggregateStatsDf(filename, df_type):
     working_df = getCompoundDf(filename) if df_type == 'compound' else getRatiosDf(filename)
-    descriptive_stats_ls = [] #this one is just describing every BR/compound/treament combo
+    descriptive_stats_ls = [] #this one is just describing every region/compound/treament combo
     proper_stats_ls = [] #this one really comparing groups
 
-    for BR_comp, BR_comp_groupby_df in working_df.groupby(by =['BR', 'compound']):
-        for treatment, treatment_groupby_df in BR_comp_groupby_df.groupby(by =['treatment']):
+    for region_comp, region_comp_groupby_df in working_df.groupby(by =['region', 'compound']):
+        for treatment, treatment_groupby_df in region_comp_groupby_df.groupby(by =['treatment']):
             F, p, is_valid = [*scipy.stats.shapiro(treatment_groupby_df['ng_mg']), True] if len(treatment_groupby_df) >= 3 else [np.NaN, np.NaN, False]
             mean, std, sem, values = [treatment_groupby_df.ng_mg.mean(), treatment_groupby_df.ng_mg.std(), treatment_groupby_df.ng_mg.sem(), list(treatment_groupby_df.ng_mg)]  
-            descriptive_stats_ls.append([*[treatment, *BR_comp], F, p, is_valid, mean, std, sem, values]) #start unpacks the list of strings
+            descriptive_stats_ls.append([*[treatment, *region_comp], F, p, is_valid, mean, std, sem, values]) #start unpacks the list of strings
             
         f_one, p_one = scipy.stats.f_oneway(g1, g2, g3) #pseudo code
         f_two, p_two = scipy.stats.f_twoway(g1, g2, g3) #pseudo code
-        proper_stats_ls.append([treatment, BR_comp, F, p]) #pseudo code 
+        proper_stats_ls.append([treatment, region_comp, F, p]) #pseudo code 
         
-    return pd.DataFrame(descriptive_stats_ls, columns= ['treatment', 'BR', 'compound', 'shapiro_F', 'shapiro_p', 'is_valid', 'mean', 'std', 'sem', 'values'])
+    return pd.DataFrame(descriptive_stats_ls, columns= ['treatment', 'region', 'compound', 'shapiro_F', 'shapiro_p', 'is_valid', 'mean', 'std', 'sem', 'values'])
 
 ############### CALCULATE/STATISTICS ############
+### The following functions are just here to be passed to the pd.corr() method, c and y are the two lists of values (columns) to be correlated
+def isSignificant(stat_method_cb, pval_threshold): #This is a classic design pattern of which i forget the name again. 
+# As you can see here it will return a function. NOT CALL THE FUNCTION, but return it. The point here is to inject variables in the function that is returned.
+# when isSignificant(callback, pval_threshold) is called, it declare the anonymous function (lambda) passing the variables, and returns this declaration
+# this means that when the lambda function is called later on these will be 'harcoded' in the sense that they are no longer variables to be passed
+# Why? because if you look at the usage I pass it to pd.corr() to generate the mask in getPearsonCorrStats(). The thing is that pd.corr() calls the method it is given with (x, y) arguments
+# For the mask to be properly generated however, we also want to give a pval threshold to pass, as well as the statistical method that determines significance
+# But there is no way to pass this once you are in th pd.corr() context, so this is why you use this design pattern. It is meant to import variable into a context where they are not normally available
+    return lambda x, y: stat_method_cb(x, y) >= pval_threshold 
 
-def isSignificant(stat_method_cb, threshold):
-    return lambda x, y: stat_method_cb(x, y) >= threshold
-
+### The 
 def getPearson(x,y):
         return scipy.stats.pearsonr(x,y)
         
@@ -259,35 +248,52 @@ def getPearsonR(x,y):
 
 def getPearsonPValue(x,y):
         return getPearson(x,y)[1]
+#### Up to here ####
 
+
+#### Here we actually do the proper stats, which include reorganizing the df so that its readily usable
 def getPeasonCorrStats(df, pivot_column, p_value_threshold, n_minimum):
-    methods = [getPearsonR, isSignificant(getPearsonPValue, p_value_threshold)]
-    pivot_df = df.pivot_table(values='ng_mg', index=df['mouse_id'], columns=pivot_column).filter(COLUMN_ORDER[pivot_column])
-    correlogram_df, p_value_mask = [pivot_df.corr(method=method, min_periods=n_minimum).dropna(axis=0, how='all').dropna(axis=1, how='all') for method in methods]
-    return correlogram_df, p_value_mask.astype(bool)
+    methods = [getPearsonR, isSignificant(getPearsonPValue, p_value_threshold)] #This is the list of methods to pass to df.corr. I know you used to pass 'pearson' as a string but this way the R calculation and pvalue mask are 'linked' by being done on the same line
+    pivot_df = df.pivot_table(values='ng_mg', index=df['mouse_id'], columns=pivot_column).filter(COLUMN_ORDER[pivot_column]) #Here we just picot our structure to one where each column is a region or compound and each line a mouse
+    correlogram_df, p_value_mask = [pivot_df.corr(method=method, min_periods=n_minimum).dropna(axis=0, how='all').dropna(axis=1, how='all') for method in methods] # ANd finally we calculate the R values and the pvalue mask in a for loop becaus the go through the exact same treatment
+    return correlogram_df, p_value_mask.astype(bool) # Convert 1s and 0s to boolean for the plotting func
+
+def getSubselection(df, selector):
+    query = []
+    for key, values in selector.items():
+        query += [f"{key}=='{value}'" for value in values]
+    return df.query('|'.join(query))
 
 def getAndPlotCorrelograms(filename, selector, p_value_threshold=0.05, n_minimum=5): #TODO: Improve performance cuz this is slow AF
     compound_df = getCompoundDf(filename)
-    for column, values in selector.items():
+    for column, values in selector.items(): #Iterate through the selector dict
         values = [values] if type(values) is str else values
-        subselection_df = pd.concat([compound_df[compound_df[column] == value] for value in values])
-        for grouping_name, col_groupby_df in subselection_df.groupby(by=[column]):
-            for treatment, groupby_df in col_groupby_df.groupby(by=['treatment']):
-                pivot_column = 'BR' if column == 'compound' else 'compound'
-                correlogram_df, p_value_mask = getPeasonCorrStats(groupby_df, pivot_column, p_value_threshold, n_minimum)
-                plotCorrelogram(correlogram_df, p_value_mask, grouping_name[0], treatment[0])
-            
-def plotCorrelogram(correlogram_df, p_value_mask, grouping_name, treatment):
-    fig, ax = plt.subplots(figsize=(16, 10))
-    # mask = np.invert(np.tril(p_value_mask < 0.05)) #generalise p_value = 0.05
+        compound_df = getSubselection(compound_df, selector)
+        # subselection_df = pd.concat([compound_df[compound_df[column] == value] for value in values])
+        for grouping_name, column_groupby_df in compound_df.groupby(by=[column]): #TODO group all graph of on experiment
+            for experiment, experiment_groupby_df in column_groupby_df.groupby(by=['experiment']): 
+                to_plot = []
+                for treatment, treatment_groupby_df in experiment_groupby_df.groupby(by=['treatment']): 
+                    pivot_column = 'region' if column == 'compound' else 'compound'
+                    # correlogram_df, p_value_mask = getPeasonCorrStats(treatment_groupby_df, pivot_column, p_value_threshold, n_minimum)
+                    # plotCorrelogram(correlogram_df, p_value_mask, f"{grouping_name[0]} in {treatment[0]}")
+                    to_plot.append([*getPeasonCorrStats(treatment_groupby_df, pivot_column, p_value_threshold, n_minimum), f"{grouping_name[0]} in {treatment[0]}"])
+                plotCorrelograms(to_plot)
+                
 
-    heatmap = sns.heatmap(correlogram_df, vmin=-1, vmax=1, annot=True, cmap='BrBG', mask=p_value_mask, annot_kws={"size": 8})
-    heatmap.set_title(f"{grouping_name} in {treatment}", fontdict={'fontsize': 18}, pad=12)
+def plotCorrelograms(correlograms):
+    fig, axs = plt.subplots(2, 2, figsize=(10,5))
+    axs = list(itertools.chain.from_iterable(axs)) # Put all the axes into a list at the same level
+    for (correlogram_df, p_value_mask, title), ax in zip(correlograms, axs):
+        plotCorrelogram(correlogram_df, p_value_mask, title, ax)
+    fig #plot
+
+def plotCorrelogram(correlogram_df, p_value_mask, title, ax):
+    heatmap = sns.heatmap(correlogram_df, vmin=-1, vmax=1, annot=True, cmap='BrBG', mask=p_value_mask, annot_kws={"size": 8}, ax=ax)
+    heatmap.set_title(title, fontdict={'fontsize': 10}, pad=20)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
     ax.set_ylabel('')
     ax.set_xlabel('')
-    return fig
-        
         
 def plotAnything(anything):
     return plt(anything)
