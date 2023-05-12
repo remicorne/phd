93121d8af46b2eca5b77f044a4b01f849a03f37d
remicorne <remi.corne@gmail.com>
2023-05-12 16:18:44 +0200

    finished square correlograms and generalized correlogram method

--- HPLC_MODULE_NOTEBOOK.py
+++ HPLC_MODULE_NOTEBOOK.py
@@ -179,7 +179,7 @@ def buildCompoundDf(filename):
     new_format_df = raw_df.melt(id_vars=['mouse_id', 'group_id'], value_vars=raw_df.columns[2:])
     new_format_df[['compound', 'region']] = new_format_df.apply(lambda x: x.variable.split('_'), axis=1, result_type='expand')
     new_format_df = applyTreatmentMapping(new_format_df, filename)
-    return new_format_df.rename(columns={'value': 'ng_mg'}).drop(columns=['variable'])
+    return new_format_df.drop(columns=['variable'])
 
 #Contains the logic to build the ratios df based on the df with the new format
 def buildRatiosDf(filename):
@@ -188,8 +188,8 @@ def buildRatiosDf(filename):
     merged = merged[~((merged.region_1 == merged.region_2) & (merged.compound_1 == merged.compound_2))] #eliminate duplicates (region1 == region2 & C1 == C2)
     # merged = merged[(merged.region_1 < merged.region_2) | (merged.compound_1 < merged.compound_2) #Eliminate duplicates (region1 == region2 & C1 == C2) and cross combinations (row1: region1=A, C1=B, region2=C, C2=D; row2: region1=C, C1=D, region2=A, C2=B))
     #         & ~((merged.region_1 > merged.region_2) & (merged.compound_1 < merged.compound_2))] #Uncomment code to only save half the ration (C1/C2) to divide time it takes by 2
-    merged[['compounds', 'ratio']] = merged.apply(lambda x: [f'{x.compound_1}/{x.compound_2}', x.ng_mg_1 / x.ng_mg_2 if x.ng_mg_2 else np.NaN], axis=1, result_type='expand') #calculate the ratio
-    return merged.rename(columns={'treatment_1': 'treatment'}).drop(columns=['treatment_2', 'ng_mg_1', 'ng_mg_2']) #Drop duplicate columns
+    merged[['compounds', 'ratio']] = merged.apply(lambda x: [f'{x.compound_1}/{x.compound_2}', x.value_1 / x.value_2 if x.value_2 else np.NaN], axis=1, result_type='expand') #calculate the ratio
+    return merged.rename(columns={'treatment_1': 'treatment'}).drop(columns=['treatment_2', 'value_1', 'value_2']) #Drop duplicate columns
 
 #Function to get the specific rations (intra region) that you use based on a ratios dictionnary
 def buildRatiosPerRegionDf(filename, ratios_mapping):
@@ -205,8 +205,8 @@ def buildAggregateStatsDf(filename, df_type):
     working_df = getCompoundDf(filename) if df_type == 'compound' else getRatiosDf(filename)
     result_ls = []
     for treat_region_comp, groupby_df in working_df.groupby(by =['treatment', 'region', 'compound']):
-        F, p, is_valid = [*scipy.stats.shapiro(groupby_df['ng_mg']), True] if len(groupby_df) >= 3 else [np.NaN, np.NaN, False]
-        mean, std, sem, values = [groupby_df.ng_mg.mean(), groupby_df.ng_mg.std(), groupby_df.ng_mg.sem(), list(groupby_df.ng_mg)]  
+        F, p, is_valid = [*scipy.stats.shapiro(groupby_df['value']), True] if len(groupby_df) >= 3 else [np.NaN, np.NaN, False]
+        mean, std, sem, values = [groupby_df.value.mean(), groupby_df.value.std(), groupby_df.value.sem(), list(groupby_df.value)]  
         result_ls.append([*treat_region_comp, F, p, is_valid, mean, std, sem, values]) #start unpacks the list of strings
     return pd.DataFrame(result_ls, columns= ['treatment', 'region', 'compound', 'shapiro_F', 'shapiro_p', 'is_valid', 'mean', 'std', 'sem', 'values'])
 
@@ -218,8 +218,8 @@ def testBuildAggregateStatsDf(filename, df_type):
 
     for region_comp, region_comp_groupby_df in working_df.groupby(by =['region', 'compound']):
         for treatment, treatment_groupby_df in region_comp_groupby_df.groupby(by =['treatment']):
-            F, p, is_valid = [*scipy.stats.shapiro(treatment_groupby_df['ng_mg']), True] if len(treatment_groupby_df) >= 3 else [np.NaN, np.NaN, False]
-            mean, std, sem, values = [treatment_groupby_df.ng_mg.mean(), treatment_groupby_df.ng_mg.std(), treatment_groupby_df.ng_mg.sem(), list(treatment_groupby_df.ng_mg)]  
+            F, p, is_valid = [*scipy.stats.shapiro(treatment_groupby_df['value']), True] if len(treatment_groupby_df) >= 3 else [np.NaN, np.NaN, False]
+            mean, std, sem, values = [treatment_groupby_df.value.mean(), treatment_groupby_df.value.std(), treatment_groupby_df.value.sem(), list(treatment_groupby_df.value)]  
             descriptive_stats_ls.append([*[treatment, *region_comp], F, p, is_valid, mean, std, sem, values]) #start unpacks the list of strings
             
         f_one, p_one = scipy.stats.f_oneway(g1, g2, g3) #pseudo code
@@ -229,8 +229,12 @@ def testBuildAggregateStatsDf(filename, df_type):
     return pd.DataFrame(descriptive_stats_ls, columns= ['treatment', 'region', 'compound', 'shapiro_F', 'shapiro_p', 'is_valid', 'mean', 'std', 'sem', 'values'])
 
 ############### CALCULATE/STATISTICS ############
+
+STAT_METHODS = {'pearson': []}
+
+
 ### The following functions are just here to be passed to the pd.corr() method, c and y are the two lists of values (columns) to be correlated
-def isSignificant(stat_method_cb, pval_threshold): #This is a classic design pattern of which i forget the name again. 
+def isSignificant(stat_method_cb, pval_threshold = 0.05): #This is a classic design pattern of which i forget the name again. 
 # As you can see here it will return a function. NOT CALL THE FUNCTION, but return it. The point here is to inject variables in the function that is returned.
 # when isSignificant(callback, pval_threshold) is called, it declare the anonymous function (lambda) passing the variables, and returns this declaration
 # this means that when the lambda function is called later on these will be 'harcoded' in the sense that they are no longer variables to be passed
@@ -254,46 +258,46 @@ def getPearsonPValue(x,y):
 #### Here we actually do the proper stats, which include reorganizing the df so that its readily usable
 def getPeasonCorrStats(df, pivot_column, p_value_threshold, n_minimum):
     methods = [getPearsonR, isSignificant(getPearsonPValue, p_value_threshold)] #This is the list of methods to pass to df.corr. I know you used to pass 'pearson' as a string but this way the R calculation and pvalue mask are 'linked' by being done on the same line
-    pivot_df = df.pivot_table(values='ng_mg', index=df['mouse_id'], columns=pivot_column).filter(COLUMN_ORDER[pivot_column]) #Here we just picot our structure to one where each column is a region or compound and each line a mouse
+    pivot_df = df.pivot_table(values='value', index=df['mouse_id'], columns=pivot_column).filter(COLUMN_ORDER[pivot_column]) #Here we just picot our structure to one where each column is a region or compound and each line a mouse
     correlogram_df, p_value_mask = [pivot_df.corr(method=method, min_periods=n_minimum).dropna(axis=0, how='all').dropna(axis=1, how='all') for method in methods] # ANd finally we calculate the R values and the pvalue mask in a for loop becaus the go through the exact same treatment
     return correlogram_df, p_value_mask.astype(bool) # Convert 1s and 0s to boolean for the plotting func
 
-def getSubselection(df, selector):
-    query = []
-    for key, values in selector.items():
-        query += [f"{key}=='{value}'" for value in values]
-    return df.query('|'.join(query))
 
 def getAndPlotCorrelograms(filename, selector, p_value_threshold=0.05, n_minimum=5): #TODO: Improve performance cuz this is slow AF
     compound_df = getCompoundDf(filename)
     for column, values in selector.items(): #Iterate through the selector dict
-        values = [values] if type(values) is str else values
-        compound_df = getSubselection(compound_df, selector)
-        # subselection_df = pd.concat([compound_df[compound_df[column] == value] for value in values])
-        for grouping_name, column_groupby_df in compound_df.groupby(by=[column]): #TODO group all graph of on experiment
-            for experiment, experiment_groupby_df in column_groupby_df.groupby(by=['experiment']): 
+        for value in values:
+            subvalues = value.split('-')
+            subselection_df = compound_df.query('|'.join([f"{column}=='{subvalue}'" for subvalue in subvalues]))
+            for experiment, experiment_groupby_df in subselection_df.groupby(by=['experiment']): 
                 to_plot = []
-                for treatment, treatment_groupby_df in experiment_groupby_df.groupby(by=['treatment']): 
-                    pivot_column = 'region' if column == 'compound' else 'compound'
-                    # correlogram_df, p_value_mask = getPeasonCorrStats(treatment_groupby_df, pivot_column, p_value_threshold, n_minimum)
-                    # plotCorrelogram(correlogram_df, p_value_mask, f"{grouping_name[0]} in {treatment[0]}")
-                    to_plot.append([*getPeasonCorrStats(treatment_groupby_df, pivot_column, p_value_threshold, n_minimum), f"{grouping_name[0]} in {treatment[0]}"])
+                for treatment, group_df in experiment_groupby_df.groupby(by=['treatment']): 
+                    pivot_columns = {'region':['region', 'compound'], 'compound': ['compound', 'region']}[column]
+                    pivot_df = group_df.pivot_table(values='value', index=group_df['mouse_id'], columns=pivot_columns) #Here we just picot our structure to one where each column is a region or compound and each line a mouse    correlogram_df, p_value_mask = [pivot_df.corr(method=method, min_periods=n_minimum).dropna(axis=0, how='all').dropna(axis=1, how='all') for method in methods] # ANd finally we calculate the R values and the pvalue mask in a for loop becaus the go through the exact same treatment
+                    methods = [getPearsonR, isSignificant(getPearsonPValue, p_value_threshold)] #This is the list of methods to pass to df.corr. I know you used to pass 'pearson' as a string but this way the R calculation and pvalue mask are 'linked' by being done on the same line
+                    correlogram_df, p_value_mask = [pivot_df.corr(method=method, min_periods=n_minimum).loc[tuple(subvalues)].dropna(axis=0, how='all').dropna(axis=1, how='all') for method in methods] # ANd finally we calculate the R values and the pvalue mask in a for loop becaus the go through the exact same treatment
+                    to_plot.append([correlogram_df, p_value_mask.astype(bool), treatment[0], subvalues])
                 plotCorrelograms(to_plot)
                 
 
 def plotCorrelograms(correlograms):
     fig, axs = plt.subplots(2, 2, figsize=(10,5))
     axs = list(itertools.chain.from_iterable(axs)) # Put all the axes into a list at the same level
-    for (correlogram_df, p_value_mask, title), ax in zip(correlograms, axs):
-        plotCorrelogram(correlogram_df, p_value_mask, title, ax)
+    for (correlogram_df, p_value_mask, treatment, subvalues), ax in zip(correlograms, axs):
+        plotCorrelogram(correlogram_df, p_value_mask, treatment, subvalues, ax)
     fig #plot
 
-def plotCorrelogram(correlogram_df, p_value_mask, title, ax):
+def plotCorrelogram(correlogram_df, p_value_mask, treatment, subvalues, ax):
     heatmap = sns.heatmap(correlogram_df, vmin=-1, vmax=1, annot=True, cmap='BrBG', mask=p_value_mask, annot_kws={"size": 8}, ax=ax)
-    heatmap.set_title(title, fontdict={'fontsize': 10}, pad=20)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
-    ax.set_ylabel('')
-    ax.set_xlabel('')
+    if len(subvalues) == 1: 
+        heatmap.set_title(f"{subvalues} in {treatment}", fontdict={'fontsize': 10}, pad=20)
+        ax.set_ylabel('')
+        ax.set_xlabel('')
+    elif len(subvalues) == 2:
+        heatmap.set_title(f"{subvalues[0]} in {treatment}", fontdict={'fontsize': 10}, pad=20)
+        ax.set_ylabel(subvalues[0])
+        ax.set_xlabel(subvalues[1])
         
 def plotAnything(anything):
     return plt(anything)
