9da3555ad838469d4ef7440a9b115cf8b0743e5d
remicorne <remi.corne@gmail.com>
2023-09-21 13:08:01 +0200

    cleanup before merge to master

--- hacky_shit.py
+++
@@ -1,9 +0,0 @@
-
-
-#FETCHING DF's
-
-# compound = getCompoundDf(filename)
-# compound[(compound.compound == 'TAU') & (compound.region == 'vH') & (compound.group_id == 1) & (compound.experiment)]
-
-# data = getRawDf(filename)
-# data[data.group_id == 1]['TAU_vH']
