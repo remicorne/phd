ea1f09ad45bff4c9ef230bf812be7d5bf2a79d30
remicorne <remi.corne@gmail.com>
2023-09-22 12:15:43 +0200

    import

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+######## ONLY IMPORT THE USER ACCESSIBLE FUNCTIONS HERE ##########
+
 from module.quantitative import quantitativeHistogram
 from module.correlogram import correlogram
 from module.utils import initiateFileSystem
 from module.metadata import saveMetadata
+from module.utils import subselectDf
+from module.getters import getCompoundAndRatiosDf
 
 ######## INIT ##########
 # Start by checking filesystem has all the folders necessary for read/write operations (cache) or create them otherwise
