da242b2fab2fc47456056497cf7a75029eaaaa06
jjb-hub <120100465+jjb-hub@users.noreply.github.com>
2023-09-28 16:02:11 +0200

    removed crap

--- module/headtwitch.py
+++ module/headtwitch.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import pandas as pd
-from module.getters import getHeadTwitchDf, getTreatmentMapping
+from module.quantitative import processQuantitativeStats
+from module.getters import getHeadTwitchDf, getExperimentalInfo
 from module.histogram import buildHeadTwitchHistogramData, buildHistogram
 from module.metadata import applyTreatmentMapping
 from module.utils import get_or_add
@@ -21,10 +22,23 @@ def headTwitchHistogram(
 ):
     HT_data=getHeadTwitchDf(HT_filename)
     data, order, palette = buildHeadTwitchHistogramData(HT_filename, experiment, vairable)
-
+    
     title = vairable.replace('_', ' at ') + ' min'
     ylabel = 'events / min'
+
     #REMI add stats calcultaion to input to histogram builder and outlier same as for quantativeHistograms()
-    fig = buildHistogram(title, ylabel, data, order, palette, hue=None, significance_infos=None)
+    # the last quantitative test is coded to return the labels directly, thus the need for the bool
+    (is_significant,
+    significance_infos,
+    ) = processQuantitativeStats(getExperimentalInfo(HT_filename)[experiment], data, p_value_threshold)
+
+    fig = buildHistogram(title, 
+                         ylabel, 
+                         data, 
+                         order, 
+                         palette, 
+                         hue=None, 
+                         significance_infos=significance_infos if is_significant else None
+                         )
     return fig
 
