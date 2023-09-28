263a23eda3eb2373c9e098a6e291889f67f61b9d
jjb-hub <120100465+jjb-hub@users.noreply.github.com>
2023-09-28 14:58:04 +0200

    checkIdentifiers() func added to change non-allowed charecters to _

--- module/utils.py
+++ module/utils.py
@@ -8,6 +8,7 @@ import sys
 import warnings
 from module.constants import *
 from PIL import Image
+import re
 
 # This function saves dictionnaries, JSON is a dictionnary text format that you use to not have to reintroduce dictionnaries as variables
 
@@ -96,12 +97,9 @@ def saveHistogram(fig, identifier):
     saveFigure(fig, identifier, "histograms")
 
 
-def saveHeadTwitchHistogram(fig, identifier):
-    saveFigure(fig, identifier, "head_twitch_histograms")
-
 
 def saveQuantitativeSummaryFig(fig, identifier):
-    saveFigure(fig, identifier, "quantitative_summary_fig")
+    saveFigure(fig, identifier, "quantitative_summary")
 
 
 def dictToFilename(dict_to_stringify):
@@ -202,7 +200,8 @@ def select_params(stat_function):
 IDENTIFIERS = {
     "histogram": 'f"{experiment}_for_{compound}_in_{region}"',
     "correlogram": 'f"{experiment}_{correlogram_type}_{buildCorrelogramFilenmae(to_correlate, columns)}"',
-    "head_twitch_histogram": 'f"head_twitch_histogram_{experiment}_for_{vairable}"',
+    "head_twitch_histogram": 'f"head_twitch_histogram_{experiment}_for_{vairable}"', 
+    "quantitative_summary":'f"quantitative_summary_{experiment}_for_{compound}_in_{region}"' #REMI: i tried to add this here but dont understand yet how it works
 }
 
 
@@ -216,6 +215,7 @@ def get_or_add(identifier_type):
     def decorator(builder_func):
         def wrapper(*args, **kwargs):
             identifier = buildIdentifier(identifier_type, **kwargs)
+            identifier=checkIdentifier(identifier)
             from_scratch = (
                 kwargs.get("from_scratch")
                 if kwargs.get("from_scratch") is not None
@@ -232,3 +232,11 @@ def get_or_add(identifier_type):
         return wrapper
 
     return decorator
+
+#checks for symbols that can be be save in name / 
+def checkIdentifier(identifier):
+    invalid_chars_pattern = r'[\/:*?"<>|%\&#$@!=+,;\'`~]'
+    sanitized_identifier = re.sub(invalid_chars_pattern, '_', identifier)
+    if re.search(invalid_chars_pattern, identifier):
+        print("Invalid characters in identifier, replacing with '_' ")
+    return sanitized_identifier
