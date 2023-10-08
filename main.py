3f48fa2949777bbc952ae3b49c3106215737df60
remicorne <remi.corne@gmail.com>
2023-10-08 14:38:21 +0200

    quantitativeStats

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 ######## ONLY IMPORT THE USER ACCESSIBLE FUNCTIONS HERE ##########
-from module.headtwitch import headTwitchHistogram #REMI this need sto have outliers and stats same methong as quant does
-from module.quantitative import quantitativeHistogram, percentageVehiclesFig, quantitativeSummary
+from module.headtwitch import (
+    headTwitchHistogram,
+)  # REMI this need sto have outliers and stats same methong as quant does
+from module.quantitative import (
+    quantitativeHistogram,
+    percentageVehiclesFig,
+    quantitativeSummary,
+)
 from module.correlogram import correlogram
 from module.utils import initiateFileSystem
 from module.metadata import saveMetadata
 from module.utils import subselectDf
-from module.getters import getCompoundAndRatiosDf, getAggregateStatsDf, getHeadTwitchDf, getRegionSubclassification
-
+from module.getters import (
+    getCompoundAndRatiosDf,
+    getAggregateStatsDf,
+    getHeadTwitchDf,
+    getRegionSubclassification,
+    getQuantitativeStats,
+)
 
 #
 from module.histogram import buildHeadTwitchHistogramData, buildHistogram
