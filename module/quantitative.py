3b6a86011e219fb8ebeeba447e4e40b4d6bb114f
remicorne <remi.corne@gmail.com>
2023-10-08 14:49:37 +0200

    correct 0 replaced by nan, add quantitativeStatsSaver

--- module/quantitative.py
+++ module/quantitative.py
@@ -8,8 +8,14 @@ from module.getters import (
     getExperimentalInfo,
     getTreatmentMapping,
     getRegionSubclassification,
+    updateQuantitativeStats,
 )
-from module.histogram import buildHistogram, buildHistogramData, buildHueHistogram, buildQuantitativeSummaryHistogramData
+from module.histogram import (
+    buildHistogram,
+    buildHistogramData,
+    buildHueHistogram,
+    buildQuantitativeSummaryHistogramData,
+)
 from module.outliers import OUTLIER_TESTS, processOutliers
 from module.statistics import QUANTITATIVE_STAT_METHODS
 from module.utils import (
@@ -25,6 +31,7 @@ import matplotlib.patches as mpatches
 from module.utils import subselectDf
 import seaborn as sns
 from module.correlogram import askColumnsToUser
+
 
 def doQuantitativeStatLogic(multiple_factors, multiple_treatments, paired, parametric):
     return {
@@ -114,8 +121,22 @@ def singleQuantitativeHistogram(
 
         data = data[data[eliminated_outlier_col_name] == False]
         # the last quantitative test is coded to return the labels directly, thus the need for the bool
-        (is_significant, significance_infos, passed_tests) = processQuantitativeStats(
+        is_significant, significance_infos, test_results = processQuantitativeStats(
             experiment_info, data, p_value_threshold
+        )
+
+        updateQuantitativeStats(
+            filename,
+            [
+                {
+                    "data_type": "HPLC",
+                    "experiment": experiment,
+                    "compound": compound,
+                    "region": region,
+                    **test_result,
+                }
+                for test_result in test_results
+            ],
         )
 
         # JJB ok for the title would like to have either:  " passes: twowayANOVA, onewayANOVA " oder "failed: two-way-anova"
@@ -135,21 +156,60 @@ def singleQuantitativeHistogram(
 
         return fig
 
-def quantitativeSummary(
-        filename,
-        experiment=None,
-        histogram_type=None, #chose a single compound or region
-        to_plot=None, #chose a list of regions or compounds (x-axis of hist) #JASI will this stay as list or should be single value? not complex like corr
-        p_value_threshold=None,
-        columns=None,
-        from_scratch=None ):
 
-    #get df 
+def processQuantitativeStats(experiment_info, data, p_value_threshold):
+    """_summary_
+
+    Args:
+        experiment_info (dict): experiment info mapping
+        data (df): data for signle experiment
+        p_value_threshold (float): threshold applied to all tests
+
+    Returns:
+        is_significant (bool), significance_infos (list, list): [treatment parings, pvalues]
+    """
+    multiple_factors = len(experiment_info["independant_vars"]) > 1
+    multiple_treatments = len(experiment_info["groups"]) > 1
+    paired = experiment_info["paired"]
+    parametric = experiment_info["parametric"]
+
+    tests = doQuantitativeStatLogic(
+        multiple_factors, multiple_treatments, paired, parametric
+    )
+    test_results = []
+    for test in tests:
+        is_significant, stats_results, *significance_infos = QUANTITATIVE_STAT_METHODS[
+            test
+        ](
+            data=data,
+            independant_vars=experiment_info["independant_vars"],
+            p_value_threshold=p_value_threshold,
+        )
+        print()
+        print(test.upper(), "SIGNIFICANT" if is_significant else "NOT SIGNIFICANT")
+        print(stats_results)
+        test_results.append(
+            {"test": test, "is_significant": is_significant, "result": stats_results}
+        )
+
+    return is_significant, significance_infos[0], test_results
+
+
+def quantitativeSummary(
+    filename,
+    experiment=None,
+    histogram_type=None,  # chose a single compound or region
+    to_plot=None,  # chose a list of regions or compounds (x-axis of hist) #JASI will this stay as list or should be single value? not complex like corr
+    p_value_threshold=None,
+    columns=None,
+    from_scratch=None,
+):
+    # get df
     compound_and_ratios_df = getCompoundAndRatiosDf(filename)
     experiments = getExperimentalInfo(filename)
 
-    #JASI input loop here if you like see quantitativeHistogram()
-    #prompts for inputs if not provided 
+    # JASI input loop here if you like see quantitativeHistogram()
+    # prompts for inputs if not provided
     experiment = (
         experiment
         if experiment
@@ -173,8 +233,7 @@ def quantitativeSummary(
         ).upper()
     )
     columns = (
-        columns if columns 
-        else askColumnsToUser(histogram_type, compound_and_ratios_df) 
+        columns if columns else askColumnsToUser(histogram_type, compound_and_ratios_df)
     )
 
     buildSingleQuantitativeSummary(
@@ -186,7 +245,8 @@ def quantitativeSummary(
         from_scratch=from_scratch,  # Used in decorator
     )
 
-@get_or_add('quantitative_summary')
+
+@get_or_add("quantitative_summary")
 def buildSingleQuantitativeSummary(
     filename,
     experiment=None,
@@ -195,77 +255,61 @@ def buildSingleQuantitativeSummary(
     columns=None,
     from_scratch=None,  # Used in decorator
 ):
+    title = ""  # EMPTY to feed generic plotter
+    ylabel = f"{to_plot} {'ng/mg' if '/' not in to_plot else ''} +/-98CI"
 
-    title =f'' #EMPTY to feed generic plotter
-    ylabel=f'{to_plot} ng/mg +/-98CI'
-    if '/' in to_plot:
-        ylabel=f'{to_plot} +/-98CI' #ratios are not in ng/mg
-
-    data, order, hue_order, hue_palette, value_type = buildQuantitativeSummaryHistogramData(filename, experiment, histogram_type, to_plot, columns)
+    (
+        data,
+        order,
+        hue_order,
+        hue_palette,
+        value_type,
+    ) = buildQuantitativeSummaryHistogramData(
+        filename, experiment, histogram_type, to_plot, columns
+    )
 
     #  buildHistogram() IS NOT GENERAL enough becaue of hue/outlier stuff - REMI can we combine? I tried and couldnt manage ... yet
-    fig = buildHueHistogram(title, ylabel, data, order, palette=hue_palette, x=value_type,y='value', hue='treatment', hue_order=hue_order, significance_infos=None)
-
-    return fig 
-
-
-def processQuantitativeStats(experiment_info, data, p_value_threshold):
-    """_summary_
-
-    Args:
-        experiment_info (dict): experiment info mapping
-        data (df): data for signle experiment
-        p_value_threshold (float): threshold applied to all tests
-
-    Returns:
-        is_significant (bool), significance_infos (list, list): [treatment parings, pvalues]
-    """
-    multiple_factors = len(experiment_info["independant_vars"]) > 1
-    multiple_treatments = len(experiment_info["groups"]) > 1
-    paired = experiment_info["paired"]
-    parametric = experiment_info["parametric"]
-
-    tests = doQuantitativeStatLogic(
-        multiple_factors, multiple_treatments, paired, parametric
+    fig = buildHueHistogram(
+        title,
+        ylabel,
+        data,
+        order,
+        palette=hue_palette,
+        x=value_type,
+        y="value",
+        hue="treatment",
+        hue_order=hue_order,
+        significance_infos=None,
     )
-    test_results = {}
-    for test in tests:
-        is_significant, stats_results, *significance_infos = QUANTITATIVE_STAT_METHODS[
-            test
-        ](
-            data=data,
-            independant_vars=experiment_info["independant_vars"],
-            p_value_threshold=p_value_threshold,
-        )
-        print()
-        print(test.upper(), "SIGNIFICANT" if is_significant else "NOT SIGNIFICANT")
-        print(stats_results)
-        if is_significant in test_results:
-            test_results[is_significant].append(test)
-        else:
-            test_results[is_significant] = [test]
 
-    return is_significant, significance_infos[0], test_results
+    return fig
 
 
-
-
-@get_or_add("percentage_vehicles") #REMI this should probebly be singlePercentageVehiclesFig
+@get_or_add(
+    "percentage_vehicles"
+)  # REMI this should probebly be singlePercentageVehiclesFig
 def percentageVehiclesFig(
-        filename,
-        experiment=None,
-        compound=None,
-        regions=None, #REMI i would ike this to work the same way it does for correlograms i.e. also specifying the order 
-        from_scratch=True ):
-            
-    #slice aggstats df 
-    experimental_df = subselectDf(getAggregateStatsDf(filename), {"compound": compound, "experiment": experiment })   #REMI can use completly omnce has list comprehension
-    experimental_df = experimental_df[experimental_df['region'].isin(regions)]
+    filename,
+    experiment=None,
+    compound=None,
+    regions=None,  # REMI i would ike this to work the same way it does for correlograms i.e. also specifying the order
+    from_scratch=True,
+):
+    # slice aggstats df
+    experimental_df = subselectDf(
+        getAggregateStatsDf(filename), {"compound": compound, "experiment": experiment}
+    )  # REMI can use completly omnce has list comprehension
+    experimental_df = experimental_df[experimental_df["region"].isin(regions)]
 
-    #check that COLUMN_ORDER is consistent with region_subclasification i.e. CORTEX then SUBCORT...
-    region_order = regions #TODO check that regions inputted are sorted by subclasification
-    experimental_df = experimental_df.assign(region=lambda x: pd.Categorical(x["region"], categories=region_order, ordered=True)).sort_values("region")
-
+    # check that COLUMN_ORDER is consistent with region_subclasification i.e. CORTEX then SUBCORT...
+    region_order = (
+        regions  # TODO check that regions inputted are sorted by subclasification
+    )
+    experimental_df = experimental_df.assign(
+        region=lambda x: pd.Categorical(
+            x["region"], categories=region_order, ordered=True
+        )
+    ).sort_values("region")
 
     # create a new column % of control mean/control_mean * 100
     experimental_df.loc[:, "percentage_of_vehicles"] = experimental_df.groupby(
@@ -275,37 +319,41 @@ def percentageVehiclesFig(
         * 100
     )
 
-    #melt df
+    # melt df
     plot_experimental_df = pd.melt(
         experimental_df,
         id_vars=["region", "treatment"],
         value_vars=["percentage_of_vehicles"],
     )
 
-    #palete 
+    # palete
     treatment_palette = {
-            info["treatment"]: info["color"] for number, info in getTreatmentMapping(filename).items()
-        }
+        info["treatment"]: info["color"]
+        for number, info in getTreatmentMapping(filename).items()
+    }
 
-    #BUILD
+    # BUILD
     fig, ax = plt.subplots(figsize=(12, 9))
     sns.set_style("white")
     sns.set_context("notebook")
 
-
-    # plot lines for each treatment 
+    # plot lines for each treatment
     for treatment_value in plot_experimental_df["treatment"].unique():
-        treatment_data = plot_experimental_df[plot_experimental_df["treatment"] == treatment_value]
+        treatment_data = plot_experimental_df[
+            plot_experimental_df["treatment"] == treatment_value
+        ]
         ax.plot(
             treatment_data["region"],
             treatment_data["value"],
-            marker='o',
+            marker="o",
             markersize=2,
-            linestyle='-',
+            linestyle="-",
             label=treatment_value,
-            color=treatment_palette.get(treatment_value, None),  # Use the palette if needed
+            color=treatment_palette.get(
+                treatment_value, None
+            ),  # Use the palette if needed
         )
-    
+
     # Create custom legend handles for 'treatment' with black outline
     order = [
         getTreatmentMapping(filename)[str(group)]["treatment"]
@@ -320,30 +368,35 @@ def percentageVehiclesFig(
     # Add a legend with custom handles and formatting
     ax.legend(handles=legend_handles, loc="upper left", frameon=False)
 
-
-
     # Add spans for region sub-classifications
     for subcls, info in getRegionSubclassification(filename).items():
-        regions_to_highlight = info['regions']
-        color = info['color']
+        regions_to_highlight = info["regions"]
+        color = info["color"]
 
         # Find the indices of regions to highlight
-        indices_to_highlight = [region_order.index(region) for region in regions_to_highlight]
+        indices_to_highlight = [
+            region_order.index(region) for region in regions_to_highlight
+        ]
 
         # Create a single span for the highlighted regions
         start_index = indices_to_highlight[0]
         end_index = indices_to_highlight[-1]
-        ax.axvspan(start_index - 0.5, end_index + 0.5, facecolor=color, alpha=0.1, edgecolor='none', label=subcls)
-        
-        
-        
+        ax.axvspan(
+            start_index - 0.5,
+            end_index + 0.5,
+            facecolor=color,
+            alpha=0.1,
+            edgecolor="none",
+            label=subcls,
+        )
+
         # Add a label for the region sub-classification at the midpoint of the span
         label = subcls.replace("_", " ")  # Replace underscores with spaces
         label_x = (start_index + end_index) / 2
         label_y = ax.get_ylim()[1] - 0.05 * (ax.get_ylim()[1] - ax.get_ylim()[0])
         # Split label into lines if it contains multiple words
         words = label.split()
-        lines = ["\n".join(words[i:i+2]) for i in range(0, len(words), 2)]
+        lines = ["\n".join(words[i : i + 2]) for i in range(0, len(words), 2)]
         label_text = "\n".join(lines)
         ax.text(
             label_x,
@@ -353,7 +406,7 @@ def percentageVehiclesFig(
             va="top",
             fontsize=12,
             color=color,
-            bbox=None #{'facecolor': 'white', 'edgecolor': 'none', 'alpha': 0.8}  # Add a white background for visibility
+            bbox=None,  # {'facecolor': 'white', 'edgecolor': 'none', 'alpha': 0.8}  # Add a white background for visibility
         )
 
     ax.set_xlim(-0.5, len(region_order) - 0.5)  # Set x-axis limits to edge of axspan
@@ -362,4 +415,3 @@ def percentageVehiclesFig(
     ax.set_ylabel(f"{compound} as a % of vehicles")
 
     return fig
-
