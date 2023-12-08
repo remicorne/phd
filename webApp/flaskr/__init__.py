5e8fe64db800d4fd6e3e08b386d0b9ea51459565
Benoit Roberge <broberge@ikosconsulting.com>
2023-12-08 20:39:59 +0100

    trying to fix path

--- webApp/flaskr/__init__.py
+++ webApp/flaskr/__init__.py
@@ -1,11 +1,10 @@
 import os
-import logging
 from flask import Flask
 
 
 def create_app(test_config=None):
     # create and configure the app
-    logging.error("current working dir is :" + os.getcwd())
+
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -25,13 +24,13 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    from . import db
+    from flaskr import db
     db.init_app(app)
 
-    from . import auth
+    from flaskr import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
+    from flaskr import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='dashboard')
 
