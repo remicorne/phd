a48f4bf42f3760ba6f09eafb535407c35fef4c88
remicorne <remi.corne@gmail.com>
2024-01-20 11:46:13 +0100

    correct some shit + remove self correlations

--- module/Network.py
+++ module/Network.py
@@ -11,44 +11,36 @@ class Network:
     def __post_init__(self):
         self.G = nx.MultiDiGraph() if self.matrix.is_square else nx.Graph()
         # directed edge -  to_correlate[0] --> to_correlate[1]
+        self.G.clear()
 
         self.G.add_nodes_from(
             self.matrix.corr_masked.columns.tolist()
         )  # adds every BR as a node
-        
-        angles = np.linspace(
-            0, 2 * np.pi, len(self.matrix.corr_masked.columns), endpoint=False
-        )
-        
-        self.pos = {}
-        self.edge_labels = {
-            
-        }
+        self.edge_labels = {}
         for (row, col), correlation in self.matrix.corr_masked.stack().dropna().items():
             # Add edge to the graph with edge weight and color
-            self.G.add_edge(
-                row,
-                col,
-                weight=abs(correlation),
-                color="red" if correlation > 0 else "blue",
-                arrowstyle="->",
-            )
-            
-            i = self.matrix.corr_masked.columns.tolist().index(col)
-            
-            self.pos = {
-                col: (np.cos(angles[i]), np.sin(angles[i]))
-                for i, col in enumerate(self.matrix.corr_masked.columns)
-            }
-            
-            self.edge_labels[(row, col)] =f"{correlation:.2f}"
+            # Avoid self sorrelation
+            if not(row == col and not self.is_bidirectional):
+                self.G.add_edge(
+                    row,
+                    col,
+                    weight=abs(correlation),
+                    color="red" if correlation > 0 else "blue",
+                )
+                
+                self.edge_labels[(row, col)] = f"{correlation:.2f}"
 
 
             ##### Draw the graph
         # pos = nx.spring_layout(G, seed=42)  # using a seed for consistency need allensdk working
 
-        
-        
+        angles = np.linspace(
+            0, 2 * np.pi, len(self.matrix.corr_masked.columns), endpoint=False
+        )
+        self.pos = {
+            col: (np.cos(angles[i]), np.sin(angles[i]))
+            for i, col in enumerate(self.matrix.corr_masked.columns)
+        }
 
     @property
     def is_bidirectional(self):
@@ -87,10 +79,11 @@ class Network:
         }  # Label nodes with their names
         nx.draw_networkx_labels(
             self.G, self.pos, labels=node_labels, font_size=18, ax=ax
-        )
+        )    
         nx.draw_networkx_edge_labels(
             self.G, self.pos, edge_labels=self.edge_labels, font_size=18, ax=ax
         )
+
         # Set title for the graph
         ax.set_frame_on(False)
         ax.set_title(self.get_title(), fontsize=28, pad=-10, y=1)
