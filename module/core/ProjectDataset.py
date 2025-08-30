61896fc8558f784d86d6102995a9773cf90be8d5
remicorne <remi.corne@gmail.com>
2025-08-30 14:18:58 +0200

    test data

--- module/core/ProjectDataset.py
+++ module/core/ProjectDataset.py
@@ -272,6 +272,7 @@ class ProjectDataset(
         return self
 
     def select(self, **selector):
+        print(self.data)
         self.selector = {**self.selector, **selector}
         selection = {**selector}
         # Pop it so it doesnt go through classic "select" filtering
