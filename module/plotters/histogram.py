e13fd783dde48cb9ef608b8856634388ea52575a
remicorne <remi.corne@gmail.com>
2025-10-04 19:14:16 +0200

    histo non case

--- module/plotters/histogram.py
+++ module/plotters/histogram.py
@@ -31,7 +31,7 @@ def histogram(project, request, filename=None, custom_params=None):
         statistic,
         custom_params=custom_params,
     )
-    return dataset.quantitative_statistics_table
+    return dataset.quantitative_statistics_table if "experiment" in request else None
 
 
 def summary_histogram(
@@ -90,4 +90,4 @@ def summary_histogram(
         statistics,
         custom_params=custom_params,
     )
-    return dataset.quantitative_statistics_table
+    return dataset.quantitative_statistics_table if "experiment" in request else None
